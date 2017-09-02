09d7527651de8bf5e2fb4dbd746b5c4516fcaf02
Karmasakshi Goyal <karmasakshi@Karmasakshis-MacBook-Air.local>
2017-09-02 11:03:19 +0530

    Add header and side-nav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,13 +6,18 @@ import { ClarityModule } from 'clarity-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     ClarityModule.forRoot(),
     AppRoutingModule
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  styleUrls: ['./side-nav.component.css'],
+  templateUrl: './side-nav.component.html'
+})
+export class SideNavComponent {
+
+  constructor() { }
+
+}
