5a6ae2a6b9d5c21f6f7e15b93e4812b16bcac5ca
Karmasakshi Goyal <karmasakshi@Karmasakshis-MacBook-Air.local>
2017-09-02 09:58:24 +0530

    Update dependencies. Add Clarity.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ClarityModule } from 'clarity-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ClarityModule.forRoot(),
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule { }
